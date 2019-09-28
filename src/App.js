b05f93c655e1efc2163815b24707f0534adff5f9
Collin Smith <cmsmith.tx@gmail.com>
2019-09-28 15:21:33 -0500

    resolving warnings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import Header from './components/Header';
-//import Nav from './components/Nav'
 import './App.css';
 
 
